0b25aa45850ed60f2a125422705d25e43814f4be
22P31A4232 <119730512+22P31A4232@users.noreply.github.com>
2022-12-22 20:23:06 +0530

    #include<stdio.h> int main() {     int m,n,i;     scanf("%d%d",&m,&n);     for(i=1;i<=n;i++)     {         if(m*i%n==0)         {             printf("%d",m*i);             break;         }     } }

---
+++ LCM.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,i;
+    scanf("%d%d",&m,&n);
+    for(i=1;i<=n;i++)
+    {
+        if(m*i%n==0)
+        {
+            printf("%d",m*i);        
+            break;
+        }
+    }
+}
