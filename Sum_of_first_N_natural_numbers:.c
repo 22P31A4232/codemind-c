fe51abc404d57b33c1c64f8a1badd58aa2b24064
22P31A4232 <119730512+22P31A4232@users.noreply.github.com>
2023-01-07 22:33:01 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int sum;     sum=a*(a+1)/2;     printf("%d",sum); }

--- Sum_of_first_N_natural_numbers:.c
+++ Sum_of_first_N_natural_numbers:.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int n,sum;
-    scanf("%d",&n);
-    sum=n*(n+1)*0.5;
+    int a;
+    scanf("%d",&a);
+    int sum;
+    sum=a*(a+1)/2;
     printf("%d",sum);
 }
