430e09de75165c0e9336655227100b8d2d7370e3
pulapa vanaja <119730512+22P31A4232@users.noreply.github.com>
2023-01-23 22:18:04 +0530

    #include<stdio.h> int main() {     int m,n,i,sum=0;     scanf("%d%d",&m,&n);     for(i=m;i<=n;i++)     {         if(i%3==0)         {             sum++;         }     }     printf("%d",sum); }

---
+++ count_Multiples_of_3_between_M_,N.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,i,sum=0;
+    scanf("%d%d",&m,&n);
+    for(i=m;i<=n;i++)
+    {
+        if(i%3==0)
+        {
+            sum++;
+        }
+    }
+    printf("%d",sum);
+}
