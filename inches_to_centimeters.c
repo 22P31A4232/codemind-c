f71456eaab13298264a0c77be78ae4156dae0ba9
pulapa vanaja <119730512+22P31A4232@users.noreply.github.com>
2023-01-25 20:38:29 +0530

    #include<stdio.h> int main() {     int h;     scanf("%d",&h);     float height;     height=2.54*h;     printf("%0.2f",height); }

--- inches_to_centimeters.c
+++ inches_to_centimeters.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int height;
-    scanf("%d",&height);
-    float h;
-    h=2.54*height;
-    printf("%0.2f",h);
+    int h;
+    scanf("%d",&h);
+    float height;
+    height=2.54*h;
+    printf("%0.2f",height);
 }
