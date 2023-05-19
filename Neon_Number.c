824bbc3f49b85f68417a43bd7cb43b6ef98d8d04
pulapa vanaja <119730512+22P31A4232@users.noreply.github.com>
2023-05-19 22:13:58 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,sq,r,sum=0;     scanf("%d",&n);     sq=n*n;     for(sum=0;sq>0;sq=sq/10){         r=sq%10;         sum=sum+r;     }     if(sum==n){         printf("Neon Number");     }     else{         printf("Not Neon Number");     } }

--- Neon_Number.c
+++ Neon_Number.c
@@ -2,20 +2,17 @@
 #include<math.h>
 int main()
 {
-   int n,sq,rem,sum;
-   scanf("%d",&n);
-   sq=pow(n,2);
-   for(sum=0;sq>0;sq=sq/10)
-   {
-       rem=sq%10;
-       sum=sum+rem;
-   }
-   if(sum==n)
-   {
-       printf("Neon Number");
-   }
-   else
-   {
-       printf("Not Neon Number");
-   }
+    int n,sq,r,sum=0;
+    scanf("%d",&n);
+    sq=n*n;
+    for(sum=0;sq>0;sq=sq/10){
+        r=sq%10;
+        sum=sum+r;
+    }
+    if(sum==n){
+        printf("Neon Number");
+    }
+    else{
+        printf("Not Neon Number");
+    }
 }
